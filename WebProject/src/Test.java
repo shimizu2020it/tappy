2a7fc123388de2738a776023c1d7c818d04e024f
atsu-shi <atsu-shi@NA20428.nature.local>
2020-05-25 13:48:16 +0900

    piyopiyo add

--- WebProject/src/Test.java
+++ WebProject/src/Test.java
@@ -29,6 +29,7 @@ public class Test extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("hogehoge");
+		System.out.println("piyopiyo");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
