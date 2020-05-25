640ec7a6e27547cb1ec33cea31c933d7cc758b2d
atsu-shi <atsu-shi@NA20428.nature.local>
2020-05-25 13:42:28 +0900

    20/05/25 hogehoge add

--- WebProject/src/Test.java
+++ WebProject/src/Test.java
@@ -28,6 +28,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("hogehoge");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
