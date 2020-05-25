af5a167b2b7d5ce0a8d4cf592150790ac2756f14
user558993 <bufigazu@macr2.com>
2020-05-25 14:11:39 +0900

    my unmerged commit

--- WebProject/src/Test.java
+++ WebProject/src/Test.java
@@ -28,6 +28,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("my commit");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
