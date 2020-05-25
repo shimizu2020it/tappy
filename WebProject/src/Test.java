84de834247ca46c4d86f383b29625845f574da97
d.lee <d.lee@LAPTOP-KLBV8L0R.aimtech.co.jp>
2020-05-25 14:16:20 +0900

    メッセージを追加

--- WebProject/src/Test.java
+++ WebProject/src/Test.java
@@ -27,7 +27,7 @@ public class Test extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("ee");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
